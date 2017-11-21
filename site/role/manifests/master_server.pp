5609ec79ac14b01e1a6d2eed29d31c3f01d433c3
Andrés Villalobos Rivera <aevr007@gmail.com>
2017-11-21 13:01:00 -0800

    Update master_server.pp

--- site/role/manifests/master_server.pp
+++ site/role/manifests/master_server.pp
@@ -1,5 +1,7 @@
 class role::master_server{
   include profile::base
   include profile::agent_nodes
-  include profile::ssh_server
+  if $facts['networking.ip'] == '172.18.0.2' or $facts['networking.ip'] == '172.18.0.3' {
+    include profile::ssh_server
+  }
 }
