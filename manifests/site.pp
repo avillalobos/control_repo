730ebe7ce5ae38d730b28426854b82c2d8fc618c
Andres Villalobos <avillalobos@lsst-chirino.lsst.local>
2017-11-27 18:26:15 -0300

    Added a new node for a new VM installed for testing purposes

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,26 +1,30 @@
 node default {
-  file { '/root/README' :
-    ensure => file,
-    content => "Welcome to ${fqdn}, this is a default server, uptime about ${system_uptime}\n",
-  }
+	file { '/root/README' :
+		ensure => file,
+		content => "Welcome to ${fqdn}, this is a default server, uptime about ${system_uptime}\n",
+	}
 }
 
 node /^master/ {
-  include role::master_server 
+	include role::master_server 
+}
+
+node "antares.lsst.local"{
+	include role:master_server
 }
 
 node 'app.puppet.vm' {
-  include role::app_server
+	include role::app_server
 }
 
 node 'minetest.puppet.vm' {
-  include role::minecraft_server
+	include role::minecraft_server
 }
 
 node /^web/ {
-  include role::app_server
+	include role::app_server
 }
 
 node /^db/ {
-  include role::db_server
+	include role::db_server
 }
