3bf0904d4645b3feada94ce75f6df958a90404ef
Andres Villalobos <avillalobos@lsst-chirino.lsst.local>
2017-11-30 18:51:35 -0300

    Telescope and Site development environment
    Telescope and site development environment, biasic setup.

---
+++ site/profile/manifests/ts-sw-devenv.pp
@@ -0,0 +1,48 @@
+class profile::ts-sw-devenv{
+
+	package { 'gcc-c++':
+		ensure => present,
+	}
+	package { 'make':
+		ensure => present,
+	}
+
+	package { 'ncurses-libs':
+		ensure => present,
+	}
+	package { 'xterm':
+		ensure => present,
+	}
+	package { 'xorg-x11-fonts-misc':
+		ensure => present,
+	}
+	package { 'java-1.7.0-openjdk-devel':
+		ensure => present,
+	}
+	package { 'boost-python':
+		ensure => present,
+	}
+	package { 'boost-python-devel':
+		ensure => present,
+	}
+	package { 'maven':
+		ensure => present,
+	}
+	package { 'python-devel':
+		ensure => present,
+	}
+	package { 'swig':
+		ensure => present,
+	}
+	package { 'tk-devel':
+		ensure => present,
+	}
+	package { 'wget':
+		ensure => present,
+	}
+	#exec { 'get-custom-python':
+	#	command => 'wget ftp://ftp.noao.edu/pub/dmills/python3-to-install.tgz -O /tmp/python3-to-install.tgz ; cd /tmp/ ; tar -xvf python3-to-install.tgz ; cd Python-3.6.3; make install && pip install numpy',
+	#	path => '/bin/',
+	#}
+
+}
