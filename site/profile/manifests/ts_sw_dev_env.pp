b43461d27693a5b5ac824fa054c20797bef2666b
Andres Villalobos <avillalobos@lsst-chirino.lsst.local>
2017-12-01 17:45:17 -0300

    Adding sal repo download

--- site/profile/manifests/ts_sw_dev_env.pp
+++ site/profile/manifests/ts_sw_dev_env.pp
@@ -40,9 +40,17 @@ class profile::ts_sw_dev_env{
 	package { 'wget':
 		ensure => present,
 	}
+	package { 'git':
+		ensure => present,
+	}
 	exec { 'get-custom-python':
 		command => 'wget ftp://139.229.136.22/pub/python3-to-install.tgz -O /tmp/python3-to-install.tgz ; cd /tmp/ ; tar -xvf python3-to-install.tgz ; cd Python-3.6.3; make install && /usr/local/bin/pip3 install numpy',
 		path => '/bin/',
 	}
 
+	vcsrepo { '/root/ts_sal':
+		ensure => present,
+		provider => git,
+		source => 'git://github.com/lsst-ts/ts_sal.git'
+	}
 }
