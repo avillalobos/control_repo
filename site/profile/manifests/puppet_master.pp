fc77ae0f3b8c563a9992fe2c924e237968252d40
Andres Villalobos <avillalobos@lsst-chirino.lsst.local>
2017-12-01 17:04:58 -0300

    Module installation at puppet master

--- site/profile/manifests/puppet_master.pp
+++ site/profile/manifests/puppet_master.pp
@@ -1,5 +1,12 @@
 class profile::puppet_master {
+
+	exec { 'puppet-module-manager':
+		command => 'puppet module install rcoleman/puppet_module',
+		path => '/opt/puppetlabs/bin/',
+	}
+
 	module { 'puppetlabs/vcsrepo':
 		ensure => present,
+		require => Exec['puppet-module-manager']
 	}
 }
