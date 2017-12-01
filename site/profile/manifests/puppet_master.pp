0fc8a51364142fd353f71076bba4439aee6f9db2
Andres Villalobos <avillalobos@lsst-chirino.lsst.local>
2017-12-01 17:12:47 -0300

    more changes

--- site/profile/manifests/puppet_master.pp
+++ site/profile/manifests/puppet_master.pp
@@ -2,7 +2,7 @@ class profile::puppet_master {
 
 	exec { 'puppet-module-manager':
 		command => 'puppet module install rcoleman/puppet_module',
-		path => '/opt/puppetlabs/bin/',
+		path => ['/opt/puppetlabs/bin/','/bin'],
 	}
 
 	module { 'puppetlabs/vcsrepo':
